5793f247ce970725b2f5df1ba0f352f3d425d45c
Henrique Velardo <Henrique>
2023-03-23 23:24:09 -0300

    Implementando criação de cards de colaboradores de forma dinamica com React

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -6,27 +6,20 @@ import { useState } from 'react';
 
 const Formulario = (props) => {
 
-    const times = [
-        'Programação', 
-        'Front-End',
-        'Data Science',
-        'DevOps',
-        'UX e Design', 
-        'Mobile', 
-        'Inovação e gestão'
-    ]
 
     const aoSalvar = (event) => {
         event.preventDefault();
 
         props.aoColaboradorCadastrado({
-            nome, 
-            cargo, 
-            imagem, 
-            time, 
-        }
-)
+            nome,
+            cargo,
+            imagem,
+            time,
+        })
+
     }
+
+    
 
     const [nome, setNome] = useState('')
     const [cargo, setCargo] = useState('')
@@ -37,32 +30,32 @@ const Formulario = (props) => {
         <section className="formulario">
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Nome" 
-                    placeholder="Digite seu nome" 
-                    valor = {nome}
-                    aoAlterado = {valor => setNome(valor)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={nome => setNome(nome)}
                 />
-                <CampoTexto 
-                    obrigatorio={true} 
-                    label="Cargo" 
-                    placeholder="Digite seu cargo" 
-                    valor = {cargo}
-                    aoAlterado = {cargo => setCargo(cargo)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Cargo"
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={cargo => setCargo(cargo)}
                 />
-                <CampoTexto 
-                    label="Imagem" 
-                    placeholder="Digite o endereço da imagem" 
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
                     valor={imagem}
-                    aoAlterado = {imagem => setImagem(imagem)}
+                    aoAlterado={imagem => setImagem(imagem)}
                 />
-                <ListaSuspensa 
-                    obrigatorio={true} 
-                    itens={times} 
+                <ListaSuspensa
+                    obrigatorio={true}
+                    itens={props.time}
                     label="Time"
-                    valor ={time}
-                    aoAlterado = {time => setTime(time)}
+                    valor={time}
+                    aoAlterado={time => setTime(time)}
                 />
                 <Botao >
                     Cria Card
